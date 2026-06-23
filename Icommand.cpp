5c0a4100fa2d46175ef06dd0b8957ee2e1fa0ebf
慎 <127825257+Sh1nNoShin@users.noreply.github.com>
2026-06-23 21:18:48 +0800

    added basic Icommands(print, add, declare, subtract)
    oops, i must have overwritten yours with mine so this is the original one

--- Icommand.cpp
+++ Icommand.cpp
@@ -1,89 +1,97 @@
+#pragma once
 #include "ICommand.h"
-#include "Process.h"
 #include <iostream>
+#include <cctype>
 #include <string>
 #include <cstdint>
+#include <algorithm>
 
-// Command 1: Just prints text to the console
-class PrintCommand : public ICommand {
-private:
-    std::string message;
-public:
-    PrintCommand(std::string msg) : message(msg) {}
-
-    void execute() override {
-        std::cout << "[Output]: " << message << std::endl;
-    }
-};
-
-// Command 2: Declare 
 class DeclareCommand : public ICommand {
 private:
     std::string varName;
-    uintptr_t value;
-public:
-    DeclareCommand(const std::string& name, uint16_t val) : varName(name), value(val) {}
+    uint16_t defaultValue;
 
-    void execute() override { // Probably need to pass a process here or some other way to make this valid
-        // process.getSymbolTable().set(varName, value); // Get the symboltable of the process there
+public:
+    DeclareCommand(std::string var, uint16_t val) 
+        : varName(var), defaultValue(val) {}
+
+    void execute(SymbolTable& table) override {
+        table.set(varName, defaultValue);
     }
 };
 
-// Command 3: Addition
+class PrintCommand : public ICommand {
+private:
+    std::string literalMessage;
+    std::string varName; // Pass empty string if there's no variable to print
+
+public:
+    PrintCommand(std::string msg, std::string var = "") 
+        : literalMessage(msg), varName(var) {}
+
+    void execute(SymbolTable& table) override {
+        // NOTE: add an 'if' statement here once screen has been implemented 
+        // to check if the user is currently looking at this process's screen.
+        
+        std::cout << literalMessage;
+        if (!varName.empty()) {
+            // table.get automatically defaults to 0 if not declared!
+            std::cout << table.get(varName); 
+        }
+        std::cout << std::endl;
+    }
+};
+
 class AddCommand : public ICommand {
 private:
-    int& resultDestination; // References where to save the result
-    int a, b;
-public:
-    AddCommand(int& dest, int val1, int val2) 
-        : resultDestination(dest), a(val1), b(val2) {}
+    std::string destVar;
+    std::string operand1;
+    std::string operand2;
 
-    void execute() override {
-        resultDestination = a + b;
-    }
-};
-
-// Command 4: Subtract
-class SubCommand : public ICommand {
-private:
-    int& resultDestination; // References where to save the result
-    int a, b;
-public:
-    SubCommand(int& dest, int val1, int val2) 
-        : resultDestination(dest), a(val1), b(val2) {}
-
-    void execute() override {
-        resultDestination = a - b;
-    }
-};
-
-// Command 5: Sleep
-class SleepCommand : public ICommand {
-private:
-    uint8_t duration;
-public:
-    SleepCommand(uint8_t x) : duration(x) {}
-
-    void execute() override { // Don't have the current stuff to implement properly
-        // TODO: Some other stuff here like get the current tick
-        // process.setState(Process::WAITING); 
-        // TODO: Then after some times has pass, wake the process up
-    }
-};
-
-// Command 6: performs a for-loop, given a set/array of instructions.
-class ForCommand : public ICommand {
-private:
-    std::vector<std::shared_ptr<ICommand>> body;
-    int repeat;
-public:
-    ForCommand(std::vector<std::shared_ptr<ICommand>> b, int r) : body(b), repeat(r) {}
-
-    void execute() override {
-        for (int i = 0; i < repeat; i++) {
-            for (auto& cmd : body) {
-                cmd->execute(); // Process here?
-            }
+    // Helper: Returns raw number if it's numeric, otherwise looks it up in the table
+    uint16_t resolveValue(const std::string& token, SymbolTable& table) {
+        if (!token.empty() && std::all_of(token.begin(), token.end(), ::isdigit)) {
+            return static_cast<uint16_t>(std::stoi(token));
         }
+        return table.get(token); // Auto-declares to 0 if variable doesn't exist
+    }
+
+public:
+    AddCommand(std::string dest, std::string op1, std::string op2) 
+        : destVar(dest), operand1(op1), operand2(op2) {}
+
+    void execute(SymbolTable& table) override {
+        uint16_t val1 = resolveValue(operand1, table);
+        uint16_t val2 = resolveValue(operand2, table);
+        
+        table.set(destVar, val1 + val2);
+    }
+};
+
+class SubtractCommand : public ICommand {
+private:
+    std::string destVar;
+    std::string operand1;
+    std::string operand2;
+
+    uint16_t resolveValue(const std::string& token, SymbolTable& table) {
+        if (!token.empty() && std::all_of(token.begin(), token.end(), ::isdigit)) {
+            return static_cast<uint16_t>(std::stoi(token));
+        }
+        return table.get(token);
+    }
+
+public:
+    SubtractCommand(std::string dest, std::string op1, std::string op2) 
+        : destVar(dest), operand1(op1), operand2(op2) {}
+
+    void execute(SymbolTable& table) override {
+        uint16_t val1 = resolveValue(operand1, table);
+        uint16_t val2 = resolveValue(operand2, table);
+        
+        // Prevents underflow since uint16_t cannot be negative
+        uint16_t result = (val1 > val2) ? (val1 - val2) : 0;
+        
+        table.set(destVar, result);
     }
 };
