3e602b3fd5bc61aa43ff4ccaba1fe0f804e258b9
QAZYD <simondioresambata@gmail.com>
2026-06-26 20:13:47 +0800

    made build bat

---
+++ build.bat
@@ -0,0 +1,25 @@
+@echo off
+
+g++ main.cpp  ^
+ProcessControl.cpp  ^
+configManager.cpp ^
+FCFSScheduler.cpp ^
+RRScheduler.cpp ^
+SymbolTable.cpp ^
+IETTHeard.cpp ^
+CommandGenerator.cpp ^
+InitializeCommand.cpp ^
+ProcessLogger.cpp ^
+ScreenSpawnerCommand.cpp ^
+-I. -o main
+
+if %errorlevel% neq 0 (
+    echo Build Failed
+    pause
+    exit /b
+)
+
+echo Build Successful
+main.exe
+
+pause
